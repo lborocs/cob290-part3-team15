9ebc6e294649eee3083181d3a71750371da932b0
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-08 16:05:28 +0000

    made the box and send icon side by side

--- frontend/src/components/chat/MessageBox.jsx
+++ frontend/src/components/chat/MessageBox.jsx
@@ -1,11 +1,13 @@
 function MessageBox() {
   return (
-    <div className="form-group">
+    <span>
+    <div className="form-group d-flex justify-content-center">
         <input type="text" className= "form-control" id="messageBox" placeholder="Enter message" />
         <button type="submit" class="btn btn-primary">
           <i class="fa fa-paper-plane"></i>
         </button>
     </div>
+    </span>
   );
 }
 
