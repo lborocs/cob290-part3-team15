d5dfc180e3d91c6629fe87aa978992d7ed380a88
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-08 17:26:54 +0000

    added an event listener

--- frontend/src/components/chat/MessageBox.jsx
+++ frontend/src/components/chat/MessageBox.jsx
@@ -1,10 +1,19 @@
 import {BsSend} from 'react-icons/bs';
+import { useEffect } from 'react';
 
 function MessageBox() {
+  useEffect(() => {
+    const messageButton = document.querySelector("#submit_message_button")
+    messageButton.addEventListener("click", function(){
+      let message = document.getElementById("message_box").value;
+      console.log(message);
+      document.getElementById("message_box").value = "";
+    });
+  }, []);
   return (
     <div className="form-group d-flex justify-content-center">
-        <input type="text" className= "form-control" id="messageBox" placeholder="Enter message" />
-        <button type="submit" className="btn btn-primary"><BsSend/></button>
+        <input type="text" className= "form-control" id="message_box" placeholder="Enter message" />
+        <button type="submit" className="btn btn-primary" id="submit_message_button"><BsSend/></button>
     </div>
   );
 }
