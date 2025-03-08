ab13b40dc1da936c8522270f4773f49602bb6b26
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-08 16:29:11 +0000

    Changed it to use react icons and made it a button for sizing

--- frontend/src/components/chat/MessageBox.jsx
+++ frontend/src/components/chat/MessageBox.jsx
@@ -1,11 +1,11 @@
+import {BsSend} from 'react-icons/bs';
+
 function MessageBox() {
   return (
-    <span>
     <div className="form-group d-flex justify-content-center">
         <input type="text" className= "form-control" id="messageBox" placeholder="Enter message" />
-        <i class="bi bi-send"></i>
+        <button type="submit" className="btn btn-primary"><BsSend/></button>
     </div>
-    </span>
   );
 }
 
