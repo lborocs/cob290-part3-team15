bb2fe05fc0e836814c87d694877b4e4881221297
AitchmJay <hugh.mb.johnson@icloud.com>
2025-03-09 00:12:33 +0000

    Making environment variables slightly more unique
    Oh my god it was literally just the fact that USER is already being used and got taken, every other variable loaded......

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,10 +5,10 @@ console.log(process.env.HOST, process.env.USER, process.env.PASSWORD, process.en
 
 //Database Connection
 const database = mysql.createConnection({
-    host: process.env.HOST, 
-    user: process.env.USER, 
-    password: process.env.PASSWORD, 
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE,
 });
 
 database.connect((err) => {
