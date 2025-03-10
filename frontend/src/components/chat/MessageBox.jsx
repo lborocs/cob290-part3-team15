c801817418cdb3bd69c9264c5d1796b2fa02dab8
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-10 12:37:48 +0000

    adapted and implemented hugh's suggestion regarding react's design philisophy

--- frontend/src/components/chat/MessageBox.jsx
+++ frontend/src/components/chat/MessageBox.jsx
@@ -1,56 +1,29 @@
 import {BsSend} from 'react-icons/bs';
-import { useEffect,useState } from 'react';
+import { useState } from 'react';
 
 function MessageBox() {
-  useEffect(() => {
-    const messageButton = document.querySelector("#submit_message_button")
-    messageButton.addEventListener("click", function(){
-      let messageBox = document.getElementById("message_box");
-      let message = messageBox.value;
-      if (message === "") {
-        return;
-      }
-      console.log(message);
-      messageBox.value = "";
-    });
-  }, []);
-  return (
-    <div className="form-group d-flex justify-content-center">
-        <input type="text" className= "form-control" id="message_box" placeholder="Enter message" />
-        <button type="submit" className="btn btn-primary" id="submit_message_button"><BsSend/></button>
-    </div>
-  );
-}
-
-
-/*Here's an example of a reacty way to do it (Not sure if it's even good practice but this was how my old project would do it) - Hugh */
-
-/*
-function MessageBox() {
-  //A use state to hold the active data in the message input
-  const [message, setMessage] = useState("");
-
-  //Function to handle updates
+  
+  // Initialize use state, which contains the inital state and the function to update the state
+  const [message, SetMessage] = useState("");
+  // Update handler function
   const handleMessageChange = (newMessage) => {
-    setMessage(newMessage);
+    SetMessage(newMessage);
   }
 
-  //Same as yours, just a function to execute on submit
+  // onSubmit function
   const onSubmit = () => {
     if (message===""){
       return
     }
     console.log(message);
-    setMessage("");
+    SetMessage(message);
   }
-
-  return(
+  return (
     <div className="form-group d-flex justify-content-center">
-      <input type="text" className="form-control" value={message} onChange={(e) => handleMessageChange(e.target.value)} placeholder="Enter Message" />
-      <button type="button" className="btn btn-primary" onClick={onSubmit}><BsSend/></button>
+        <input type="text" className= "form-control" id="message_box" value={message} onChange={(e) => handleMessageChange(e.target.value)} placeholder="Enter message" />
+        <button type="submit" className="btn btn-primary" id="submit_message_button" onClick={onSubmit}><BsSend/></button>
     </div>
-  )
+  );
 }
-*/
 
 export default MessageBox;
