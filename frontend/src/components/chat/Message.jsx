2159d40ec559d676670c3c837840295722b01ea4
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-13 18:17:27 +0000

    changed to justify

--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -2,7 +2,7 @@ function Message({ message , userID }) {
   const sentByUser = parseInt(message.user) === parseInt(userID); // Check if the message was sent by the user, parses as int and uses base 10 (denary/decimal)
   console.log(sentByUser);
   return (
-    <div className={`w-75 my-2 rounded-lg border border-2 px-5 py-2 text-base font-medium ${sentByUser ? 'text-right bg-blue-300' : 'text-left bg-gray-500'}`} id="message">
+    <div className={`w-75 my-2 rounded-lg border border-2 px-5 py-2 text-base font-medium ${sentByUser ? 'justify-right bg-blue-300' : 'justify-left bg-gray-500'}`} id="message">
         <div className="message_user" id="message_user">
             {message.user}
         </div>
