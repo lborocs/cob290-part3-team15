940b1e5da7622e383dce8f1eb49ace05c26f56de
AitchmJay <hugh.mb.johnson@icloud.com>
2025-03-13 21:53:45 +0000

    Slight tidy up

--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -1,11 +1,13 @@
 function SelfMessage({ message }) {
   return(
   <div className={`max-w-3/4 my-2 rounded-lg border border-2 border-blue-200 px-4 py-2 text-base font-medium self-end bg-blue-300`} id="message">
-    <div className="self-start w-fit text-pretty break-all">
-        {message.user}
-    </div>
-    <div className="self-start w-fit text-pretty break-all">
-        {message.content}
+    <div className="self-start text-pretty break-all">
+      <div className="w-fit">
+          {message.user}
+      </div>
+      <div className="w-fit">
+          {message.content}
+      </div>
     </div>
   </div>
   )
@@ -14,11 +16,13 @@ function SelfMessage({ message }) {
 function OtherMessage({ message }) {
   return(
   <div className={`max-w-3/4 my-2 rounded-lg border border-2 border-gray-400 px-4 py-2 text-base font-medium self-start bg-gray-500`} id="message">
-    <div className="text-left w-fit text-pretty break-all">
-        {message.user}
-    </div>
-    <div className="text-left w-fit text-pretty break-all">
-        {message.content}
+    <div className="text-left text-pretty break-all">
+      <div className="w-fit">
+          {message.user}
+      </div>
+      <div className="w-fit">
+          {message.content}
+      </div>
     </div>
   </div>
   )
