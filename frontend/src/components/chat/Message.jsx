8952f8106667e0626850612f10cfc3f8e9d966f7
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-13 17:36:01 +0000

    change message box to tailwind and added conditional display left/right

--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -1,10 +1,12 @@
-function Message({ message }) {
+function Message({ message , userID }) {
+  const sentByUser = parseInt(message.user) === parseInt(userID); // Check if the message was sent by the user, parses as int and uses base 10 (denary/decimal)
+  console.log(sentByUser);
   return (
-    <div className="message">
-        <div className="message_user">
+    <div className={`w-75 my-2 rounded-lg border border-2 px-5 py-2 text-base font-medium ${sentByUser ? 'text-right bg-blue-300' : 'text-left bg-gray-500'}`} id="message">
+        <div className="message_user" id="message_user">
             {message.user}
         </div>
-        <div className="message_content">
+        <div className="message_content" id="message_content">
             {message.content}
         </div>
     </div>
