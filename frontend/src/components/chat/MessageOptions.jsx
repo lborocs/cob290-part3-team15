12cd0f15c940a0d71ec0e654fa9c8ae020d72450
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-16 17:23:18 +0000

    started writing up an options dropdown for message

---
+++ frontend/src/components/chat/MessageOptions.jsx
@@ -0,0 +1,11 @@
+function MessageOptions(){
+    return (
+        <div className="">
+            <li className="">
+                <button>Edit</button>
+            </li>
+        </div>
+    )
+}
+
+export default MessageOptions; 
