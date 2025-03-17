239e1910aef63f97263c077e1615531fc85dfc95
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-17 21:04:23 +0000

    finished the main code for modal, changes needed

---
+++ frontend/src/components/chat/Modal.jsx
@@ -0,0 +1,25 @@
+import { BsX } from "react-icons/bs";
+export default function Modal(open, onClose, children){
+    return(
+        <div onClick={onClose} className={`
+            fixed inset-0 justify-center items-center
+            transiotion-colors
+            ${open ? "visible bg-black/20" : "invisible"}
+        `}>
+            <div
+                onClick={e => e.stopPropagation()} 
+                className={`
+                bg-white rounded-xl shadow p-6 transition-all
+                ${open ? "scale-100 opcaity-100" : "scale-125 opacity-0"}
+                `}>
+                    <button 
+                    className="absolute top-2 right-2 p-1 rounded-lg text-gray-500 hover:bg-gray-200 hover:text-gray-600" 
+                    onClick={onClose}
+                    >
+                    < BsX />
+                    </button>
+            {children}
+            </div>
+        </div>
+    )
+}
