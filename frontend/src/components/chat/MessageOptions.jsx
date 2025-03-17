a3bbb343003e2f3664ff279fc5b4a581eecc7ec5
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-17 20:06:55 +0000

    rearranged the buttons and also added some animations on hover

--- frontend/src/components/chat/MessageOptions.jsx
+++ frontend/src/components/chat/MessageOptions.jsx
@@ -1,11 +1,32 @@
 import { BsThreeDots, BsPencil, BsPencilFill } from "react-icons/bs";
+import { useState } from 'react';
 function MessageOptions(){
+    const [isHovered, setIsHovered] = useState(false);
+      
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+      
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     return (
-        <div className="">
-            <button class=""><BsThreeDots/></button>
-            <button class="hover: "><BsPencil/></button>
+        <div className="flex space-x-2">
+            <button 
+            // onClick={} 
+            className="p-1 hover:bg-gray-200 rounded">
+            <BsThreeDots className="text-gray-600" />
+            </button>
+            <button
+            // onClick={}
+            className="p-1 hover:bg-gray-200 rounded"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            >
+            {isHovered ? <BsPencilFill className="text-gray-600" /> : <BsPencil className="text-gray-600" />}
+          </button>
         </div>
-    )
+      )
 }
 
 export default MessageOptions; 
