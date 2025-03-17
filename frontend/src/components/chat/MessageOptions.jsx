f306d3b923f864979d0669015232cdc5cca22a18
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-17 20:53:17 +0000

    added the modal to appear and change background

--- frontend/src/components/chat/MessageOptions.jsx
+++ frontend/src/components/chat/MessageOptions.jsx
@@ -1,8 +1,10 @@
 import { BsThreeDots, BsPencil, BsPencilFill } from "react-icons/bs";
 import { useState } from 'react';
+import EditMessageModal from "./EditMessageModal";
 function MessageOptions(){
     const [isHoveredEdit, SetIsHoveredEdit] = useState(false);
     const [isHoveredOptions, SetIsHoveredOptions] = useState(false);  
+    const [openEditModal, SetOpenEditModal] = useState(false);
     const HandleMouseEnterEdit = () => {
         SetIsHoveredEdit(true);
       };
@@ -20,24 +22,31 @@ function MessageOptions(){
     };
 
     return (
-        <div className="flex space-x-2 justify-end">
-            <button 
-            // onClick={} 
-            className="p-1 hover: "
-            onMouseEnter={HandleMouseEnterOptions}
-            onMouseLeave={HandleMouseLeaveOptions}
-            >
-            {isHoveredOptions ? <BsThreeDots className="text-purple-200" /> : <BsThreeDots className="text-gray-200" />}
-            </button>
-            <button
-            // onClick={}
-            className="p-1 hover: "
-            onMouseEnter={HandleMouseEnterEdit}
-            onMouseLeave={HandleMouseLeaveEdit}
-            >
-            {isHoveredEdit ? <BsPencilFill className="text-purple-200" /> : <BsPencil className="text-gray-200" />}
-            </button>
-        </div>
+        <>
+            <div className="flex space-x-2 justify-end">
+                <button 
+                // onClick={} 
+                className="p-1 hover: "
+                onMouseEnter={HandleMouseEnterOptions}
+                onMouseLeave={HandleMouseLeaveOptions}
+                >
+                {isHoveredOptions ? <BsThreeDots className="text-purple-200" /> : <BsThreeDots className="text-gray-200" />}
+                </button>
+                <button
+                onClick={() => SetOpenEditModal(true)}
+                className="p-1 hover: "
+                onMouseEnter={HandleMouseEnterEdit}
+                onMouseLeave={HandleMouseLeaveEdit}
+                >
+                {isHoveredEdit ? <BsPencilFill className="text-purple-200" /> : <BsPencil className="text-gray-200" />}
+                </button>
+            </div>
+            {openEditModal && (
+            <EditMessageModal open={openEditModal} onClose={() => SetOpenEditModal(false)}>
+                <h3>Modal</h3>
+            </EditMessageModal>
+            )}
+        </>
       )
 }
 
