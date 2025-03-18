5b2f5f21351f70741b3cc31f72ba8909bf532a80
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-18 22:33:54 +0000

    designed basic edit message modal

--- frontend/src/components/chat/EditMessageModal.jsx
+++ frontend/src/components/chat/EditMessageModal.jsx
@@ -4,7 +4,12 @@ function EditMessageModal({open, onClose}){
         <>
             {open && (
                 <Modal open={open} onClose={onClose}>
-                    <h3>Edit Message</h3>
+                    <h3 className="text-lg mb-2">Edit Message</h3>
+                    <hr className="border-t-2 border-gray-300 mb-4" />
+                    <textarea className="w-full h-32 p-2 border border-gray-300 rounded-lg mb-4 resize-none" />
+                    <div className="flex justify-end mt-4">
+                        <button className="px-4 py-2 text-white bg-blue-400 rounded-lg">Save</button>
+                    </div>
                 </Modal>
             )}
         </>
