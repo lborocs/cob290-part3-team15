78d6df213f12107a945ca161f8b7ebb109d7431d
AitchmJay <hugh.mb.johnson@icloud.com>
2025-03-21 19:18:10 +0000

    Modal Z-Indexing

--- frontend/src/components/chat/Modal.jsx
+++ frontend/src/components/chat/Modal.jsx
@@ -5,7 +5,7 @@ export default function Modal({open, onClose, children}){
         onClick={onClose} 
         className={`
             fixed inset-0 flex justify-center items-center
-            transition-colors
+            transition-colors z-50
             ${open ? "visible bg-black/20" : "invisible"}
         `}>
             <div
