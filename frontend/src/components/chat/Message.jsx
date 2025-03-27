35a9f0139b24781272c42f2e7d8c647564b4b2d3
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-27 23:49:51 +0000

    changed it so edit message modal is not linked to message options being rendered to make them mutually exclusive

--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -1,6 +1,6 @@
 import MessageOptions from './MessageOptions.jsx';
 import { useState } from 'react';
-
+import EditMessageModal from './EditMessageModal.jsx';
 function Content({ message }) {
   return (
     <>
@@ -14,6 +14,7 @@ function Content({ message }) {
 
 function SelfMessage({ message }) {
   const [isHovered, SetisHovered] = useState(false); // Default is not hovered
+  const [openEditModal, SetOpenEditModal] = useState(false); // Default is not open and checks if the edit modal is open
   const HandleHover = (e) => {
     if (e.type === 'mouseenter'){
       SetisHovered(true)
@@ -30,15 +31,29 @@ function SelfMessage({ message }) {
   return(
     <div className={`max-w-3/4 my-2 rounded-lg border border-2 border-blue-200 px-4 py-2 text-base font-medium self-end bg-blue-300 relative`}onMouseEnter={HandleHover} onMouseLeave={HandleHover} onContextMenu={HandleRightClick}>
       <div className="self-start text-pretty break-all">
-        {isHovered && <MessageOptions sentByUser={true}/>}
+        {isHovered && !openEditModal && (
+          <MessageOptions sentByUser={true} 
+          isHoveredComment={isHovered}
+          SetOpenEditModal = {SetOpenEditModal}
+          />
+        )}
         <Content message={message}/>
       </div>
+      <EditMessageModal // Edit message modal only needed for self messages
+        open={openEditModal}
+        onClose={() => {
+          console.log("Closing edit modal");
+          SetOpenEditModal(false);
+          SetisHovered(false); // Reset the hover state for message options
+        }}
+      />
     </div>
   )
 }
 
 function OtherMessage({ message }) {
   const [isHovered, SetisHovered] = useState(false); // Default is not hovered
+  const [openEditModal, SetOpenEditModal] = useState(false); // Default is not open and checks if the edit modal is open
   const HandleHover = (e) => {
     if (e.type === 'mouseenter'){
       SetisHovered(true)
@@ -56,7 +71,12 @@ function OtherMessage({ message }) {
       }
       <div className={`mt-1 mb-2 rounded-lg border border-2 border-gray-400 px-4 py-2 bg-gray-500 relative`}onMouseEnter={HandleHover} onMouseLeave={HandleHover}>
         <div className="text-left flex flex-col text-pretty break-all">
-          {isHovered && <MessageOptions sentByUser={false}/>}
+          {isHovered && !openEditModal && (
+            <MessageOptions sentByUser={false} 
+            isHoveredComment={isHovered} 
+            SetOpenEditModal = {null}
+            />
+          )}
           <Content message={message}/>
         </div>
       </div>
