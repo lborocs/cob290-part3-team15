48d198493b68c23265be189c402cbd12835cb6f5
Rogger <121108364+rjansan@users.noreply.github.com>
2025-03-28 00:04:17 +0000

    changed it to pass the entire comment data

--- frontend/src/components/chat/EditMessageModal.jsx
+++ frontend/src/components/chat/EditMessageModal.jsx
@@ -1,12 +1,12 @@
 import Modal from "./Modal";
-function EditMessageModal({open, onClose, content}){ 
+function EditMessageModal({open, onClose, message}){ 
     return (
         <>
             {open && (
                 <Modal open={open} onClose={onClose}>
                     <h3 className="text-lg mb-2 justify-start">Edit Message</h3>
                     <hr className="border-t-2 border-gray-300 mb-4" />
-                    <textarea className="w-full h-32 p-2 border border-gray-300 rounded-lg mb-4 resize-none" defaultValue={content}/>
+                    <textarea className="w-full h-32 p-2 border border-gray-300 rounded-lg mb-4 resize-none" defaultValue={message.content}/>
                     <div className="flex justify-end mt-4">
                         <button className="px-4 py-2 text-white bg-blue-400 rounded-lg">Save</button>
                     </div>
