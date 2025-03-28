064c9c48cc9ff42057d261c42f668ea8a87b6de5
Tudor <93143436+T-Bufnila@users.noreply.github.com>
2025-03-28 19:55:08 +0000

    background color change

--- frontend/src/components/chat/analytics/SearchBox.jsx
+++ frontend/src/components/chat/analytics/SearchBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchBox() {
   return (
-    <div className="col-start-2 row-start-4 col-span-4 row-span-3 bg-zinc-400 rounded-3xl p-2 flex flex-col justify-center">
+    <div className="col-start-2 row-start-4 col-span-4 row-span-3 bg-gray-100 rounded-3xl p-2 flex flex-col justify-center">
         Search box
     </div>
   )
