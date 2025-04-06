293365b56ebeaa22b61a3df06a13d05650edd4a7
Rogger <121108364+rjansan@users.noreply.github.com>
2025-04-06 18:35:13 +0100

    changed it to directly called the passed function / component

--- frontend/src/components/chat/DropdownItem.jsx
+++ frontend/src/components/chat/DropdownItem.jsx
@@ -2,7 +2,7 @@ function DropdownItem({item, onClick }) {
     return (
         <div
         className="w-auto whitespace-nowrap text-sm p-2 px-4 hover:bg-gray-100 cursor-pointer rounded-md text-gray-700"
-        onClick={() => onClick(item)}
+        onClick={onClick}
         >
         {item}
         </div>
