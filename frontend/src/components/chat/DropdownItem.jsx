fe94ed0a387dc17575bb1912de27d42d51892fe5
Rogger <121108364+rjansan@users.noreply.github.com>
2025-04-06 18:24:27 +0100

    removed id again

--- frontend/src/components/chat/DropdownItem.jsx
+++ frontend/src/components/chat/DropdownItem.jsx
@@ -1,7 +1,6 @@
 function DropdownItem({item, onClick }) {
     return (
         <div
-        id="dropdown-item"
         className="w-auto whitespace-nowrap text-sm p-2 px-4 hover:bg-gray-100 cursor-pointer rounded-md text-gray-700"
         onClick={() => onClick(item)}
         >
