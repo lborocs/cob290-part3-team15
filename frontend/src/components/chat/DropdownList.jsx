3b337358eedb9eff4e3999a9a5572e21cca0b547
Rogger <121108364+rjansan@users.noreply.github.com>
2025-04-06 18:25:14 +0100

    made dropdown list map components to each item

--- frontend/src/components/chat/DropdownList.jsx
+++ frontend/src/components/chat/DropdownList.jsx
@@ -5,9 +5,12 @@ function DropdownList({ items, onClick }) {
   return (
     <div
     className="w-auto absolute bg-white rounded-lg p-2 z-50 border border-gray-300">
-        {/* Goes through each item in the list and maps items to a key value and  */}
+        {/* Goes through each item in the list and maps items to a key value*/}
         {items.map((item, index) => (
-            <DropdownItem key={index} item={item} onClick={onClick} />
+            <DropdownItem 
+            key={index} 
+            item={item} 
+            onClick={components[index]} /> // Maps the item to the component at the same index
         ))}
     </div>
   );
