1473ece4a189f3e53fc671f119bf8da0d68a4d95
Rogger <121108364+rjansan@users.noreply.github.com>
2025-04-06 18:22:24 +0100

    removed id as it has no use tbh, atleast not currently

--- frontend/src/components/chat/DropdownList.jsx
+++ frontend/src/components/chat/DropdownList.jsx
@@ -3,7 +3,8 @@ import DropdownItem from './DropdownItem';
 
 function DropdownList({ items, onClick }) {
   return (
-    <div id="dropdown-list" className="w-auto absolute bg-white rounded-lg p-2 z-50 border border-gray-300">
+    <div
+    className="w-auto absolute bg-white rounded-lg p-2 z-50 border border-gray-300">
         {/* Goes through each item in the list and maps items to a key value and  */}
         {items.map((item, index) => (
             <DropdownItem key={index} item={item} onClick={onClick} />
