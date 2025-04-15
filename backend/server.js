d987029c01b9560a24f3598bb5ce52e603d99cc5
AitchmJay <hugh.mb.johnson@icloud.com>
2025-04-15 18:09:35 +0100

    Tidied up backend slightly

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,15 @@ const app = express();
 const server = require('http').createServer(app);
 //Custom Exports
 const { authenticateSocket } = require("./exports/authenticate");
-const { setIo, addUser, removeUser, selfStatusAlert,getConnectedCount,getUserId, otherStatusAlert} = require('./exports/socket');
+const { setIo, addUser, removeUser,getConnectedCount} = require('./exports/socket');
+const { setStatus} = require('./exports/status');
 //IO Definition
 const io = require('socket.io')(server, {cors: {origin: [process.env.IP || "http://localhost:5173"]}});
 setIo(io);
 io.use(authenticateSocket);
 
 const port = process.env.PORT || 8080;
-const database = require("./config/database");
+
 
 //Connected Client Storage
 const connectedClients = {};
@@ -53,7 +54,6 @@ io.on('connection', (socket) => {
     socket.on('disconnect', async() => {
         removeUser(socket.id);
         if(getConnectedCount(userId)===0){
-            // Update user status to 'Offline' in the database asynchronously
             await setStatus(userId,"Offline",false);
         }
     });
@@ -66,75 +66,6 @@ io.on('connection', (socket) => {
         }
     });
 });
-
-async function setStatus(userId,status,loginAttempt) {
-    var query="SELECT Status FROM users WHERE UserID=? LIMIT 1;";
-    const values = [userId];
-    var currentStatus = null;
-    database.query(query, values, (err, results) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        if (results.length === 0) {
-            console.log("User not found");
-            return;
-        }
-        currentStatus = results[0].Status;
-
-        // Stop bad inputs
-        if (!currentStatus) {
-            console.log(`Status is ${currentStatus}`);
-            return;
-        }
-
-        // If this is a login attempt, Online is a lower priority than Invisible
-        if (loginAttempt && currentStatus==="Invisible"){
-            status="Invisible";
-            return;
-        }
-
-        // Update the user's status in the database
-        if (status==="Offline"){
-            query = "UPDATE users SET Status='Offline' WHERE UserID=? AND Status NOT IN ('Invisible','Offline')";
-        }
-        else if (status==="Online"){
-            query = "UPDATE users SET Status='Online' WHERE UserID=? AND Status NOT IN ('Invisible','Online')";
-        }
-        else if (status==="Invisible"){
-            query = "UPDATE users SET Status='Invisible' WHERE UserID=? AND Status NOT IN ('Invisible')";
-        }
-        else {
-            console.log("Invalid status update requested");
-            return;
-        }
-        const substitutions = {"Online": "Online", "Offline": "Offline", "Invisible": "Offline"};
-        database.query(query, values, (err, results) => {
-            if (err) {
-                console.error(err);
-            } else {
-                if(status==="Online"||status==="Invisible"){
-                    selfStatusAlert(userId,substitutions[status]);
-                }
-                if((status==="Online"||status==="Offline"||status==="Invisible") && !(currentStatus===status)){
-                    // Fetch users to alert (people who are in a chat with the user)
-                    const usersToAlertQuery= "SELECT UserID FROM active_chats WHERE Target=? AND Type='direct_messages'";
-                    var usersToAlert =[];
-                    database.query(usersToAlertQuery, [userId], (err, results) => {
-                        if (err) {
-                            console.error(err);
-                            return;
-                        }
-                        usersToAlert = results.map(row => row.UserID);
-                        otherStatusAlert(usersToAlert,substitutions[status]);
-                    });
-                    
-                }
-                console.log(`User ${userId} set to ${status}`);
-            }
-        });
-    });
-}
 
 server.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}`);
