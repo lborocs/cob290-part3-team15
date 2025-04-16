0b543649ff8c9b10e53316addee3cf1d9dc8043c
AitchmJay <hugh.mb.johnson@icloud.com>
2025-04-16 04:34:50 +0100

    Fixed local

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {target: 'http://localhost:8080', changeOrigin: true,rewrite: (path) => (path.startsWith('/api') ? path.replace(/^\/api/, '') : path)},
+      '/socket.io': {target: 'ws://localhost:8080',ws:true, changeOrigin: true},
       '/auth': {target: 'http://localhost:4000', changeOrigin: true,rewrite: (path) => (path.startsWith('/auth') ? path.replace(/^\/auth/, '') : path)}
   }},
 })
