4425133ff816091395af6ce1d860c31f4c3b9844
Rogger <121108364+rjansan@users.noreply.github.com>
2025-04-19 00:00:19 +0100

    adding modal logic

--- frontend/src/components/chat/CreateChat.jsx
+++ frontend/src/components/chat/CreateChat.jsx
@@ -1,11 +1,15 @@
 import { MdGroupAdd } from "react-icons/md";
-
+import { useState } from "react";
+import AddChatModal from "./AddChatModal.jsx";
 function CreateChat({userID}) {
+  const [addChatModal, setAddChatModal] = useState(false);
+  
   return (
     <div className="flex h-full justify-center items-center">
-      <button>
+      <button onClick = {() => setAddChatModal(true)}>
         <MdGroupAdd className="w-6 h-6 text-text/70"/>
       </button>
+      <AddChatModal open={addChatModal} onClose={() => setAddChatModal(false)} userID={userID}/>
     </div>
   );
 }
