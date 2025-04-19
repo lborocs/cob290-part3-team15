f36993bb77960db32a8a62c48cd64ecaac2a37cf
AitchmJay <hugh.mb.johnson@icloud.com>
2025-04-19 08:27:25 +0100

    SQL Update

--- backend/make_it_all.sql
+++ backend/make_it_all.sql
@@ -49,10 +49,11 @@ CREATE TABLE `active_chats` (
 LOCK TABLES `active_chats` WRITE;
 /*!40000 ALTER TABLE `active_chats` DISABLE KEYS */;
 INSERT INTO `active_chats` VALUES (1,1,'group_messages','2025-04-13 18:06:49');
-INSERT INTO `active_chats` VALUES (1,2,'direct_messages','2025-04-14 22:58:46');
-INSERT INTO `active_chats` VALUES (2,1,'direct_messages','2025-04-14 22:58:46');
+INSERT INTO `active_chats` VALUES (1,2,'direct_messages','2025-04-16 15:09:17');
+INSERT INTO `active_chats` VALUES (1,3,'direct_messages','2025-04-16 15:49:09');
+INSERT INTO `active_chats` VALUES (2,1,'direct_messages','2025-04-16 15:09:17');
 INSERT INTO `active_chats` VALUES (2,1,'group_messages','2025-04-13 18:06:49');
-INSERT INTO `active_chats` VALUES (3,1,'direct_messages','2025-03-24 06:58:29');
+INSERT INTO `active_chats` VALUES (3,1,'direct_messages','2025-04-16 15:49:09');
 /*!40000 ALTER TABLE `active_chats` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,7 +101,7 @@ CREATE TABLE `direct_messages` (
   KEY `Recipient` (`Recipient`),
   CONSTRAINT `Recipient` FOREIGN KEY (`Recipient`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Sender` FOREIGN KEY (`Sender`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +117,7 @@ INSERT INTO `direct_messages` VALUES (4,1,2,'No you don\'t!','2025-03-23 21:50:2
 INSERT INTO `direct_messages` VALUES (5,1,3,'I send you the message!','2025-03-23 23:26:33');
 INSERT INTO `direct_messages` VALUES (6,1,3,'You get message?','2025-03-23 23:27:28');
 INSERT INTO `direct_messages` VALUES (7,1,3,'I send you the message!','2025-03-23 23:37:05');
-INSERT INTO `direct_messages` VALUES (8,1,3,'Why no refresh','2025-03-23 23:37:24');
+INSERT INTO `direct_messages` VALUES (8,1,3,'Why no refresh?','2025-03-23 23:37:24');
 INSERT INTO `direct_messages` VALUES (9,1,2,'You\'re faking it...','2025-03-23 23:37:29');
 INSERT INTO `direct_messages` VALUES (10,2,1,'Test','2025-03-24 00:22:17');
 INSERT INTO `direct_messages` VALUES (11,2,1,'Hey don\'t you go hiding me','2025-03-24 05:13:42');
@@ -127,9 +128,8 @@ INSERT INTO `direct_messages` VALUES (17,2,1,'Hi just updating you on everything
 INSERT INTO `direct_messages` VALUES (18,2,1,'test','2025-04-12 14:57:06');
 INSERT INTO `direct_messages` VALUES (19,1,2,'test','2025-04-12 15:04:29');
 INSERT INTO `direct_messages` VALUES (20,1,2,'What are we testing again','2025-04-12 18:18:58');
-INSERT INTO `direct_messages` VALUES (21,1,2,'I\'ve reported you for fraud btw','2025-04-12 18:54:01');
-INSERT INTO `direct_messages` VALUES (22,2,1,'oh okay nvm','2025-04-12 18:54:58');
-INSERT INTO `direct_messages` VALUES (23,1,2,'Testing that the leave modal is working and it seems good!!','2025-04-14 22:58:46');
+INSERT INTO `direct_messages` VALUES (21,1,2,'I\'ve reported you for fraud btw.','2025-04-12 18:54:01');
+INSERT INTO `direct_messages` VALUES (22,2,1,'oh okay nvm ;-;','2025-04-12 18:54:58');
 /*!40000 ALTER TABLE `direct_messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,7 +173,7 @@ INSERT INTO `group_messages` VALUES (10,1,1,'okay it\'s fixed now','2025-04-12 1
 INSERT INTO `group_messages` VALUES (11,1,1,'test','2025-04-12 16:34:56');
 INSERT INTO `group_messages` VALUES (12,1,1,'testerrr','2025-04-12 18:12:36');
 INSERT INTO `group_messages` VALUES (13,2,1,'fixed huh?','2025-04-12 18:53:22');
-INSERT INTO `group_messages` VALUES (14,1,1,'Hate it here','2025-04-13 09:40:43');
+INSERT INTO `group_messages` VALUES (14,1,1,'Hate it here rn.','2025-04-13 09:40:43');
 /*!40000 ALTER TABLE `group_messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,6 +203,7 @@ LOCK TABLES `group_users` WRITE;
 /*!40000 ALTER TABLE `group_users` DISABLE KEYS */;
 INSERT INTO `group_users` VALUES (1,1);
 INSERT INTO `group_users` VALUES (2,1);
+INSERT INTO `group_users` VALUES (3,1);
 /*!40000 ALTER TABLE `group_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -338,9 +339,10 @@ CREATE TABLE `users` (
   `Forename` varchar(64) NOT NULL,
   `Surname` varchar(64) NOT NULL,
   `Role` enum('Manager','Employee') NOT NULL DEFAULT 'Employee',
-  `Icon` blob DEFAULT NULL,
+  `Icon` blob NOT NULL DEFAULT '[default profile icon here]',
   `PasswordHash` varchar(60) NOT NULL,
-  `Status` enum('Online','Offline','Invisible') NOT NULL DEFAULT 'Offline',
+  `Status` enum('Online','Offline','Invisible','DND','Away') NOT NULL DEFAULT 'Offline',
+  `SavedStatus` enum('Online','DND','Away') NOT NULL DEFAULT 'Online',
   PRIMARY KEY (`UserID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -351,9 +353,9 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Mr','Mime','Employee','[default profile icon here]','ABC123BCA!!!','Online');
-INSERT INTO `users` VALUES (2,'John','Smith','Manager','[default profile icon here]','ABC123BCA!!!','Invisible');
-INSERT INTO `users` VALUES (3,'Bill','Boomstick','Employee','[default profile icon here]','ABC123BCA!!!','Offline');
+INSERT INTO `users` VALUES (1,'Mr','Mime','Employee','[default profile icon here]','ABC123BCA!!!','Online','Online');
+INSERT INTO `users` VALUES (2,'John','Smith','Manager','[default profile icon here]','ABC123BCA!!!','Invisible','Online');
+INSERT INTO `users` VALUES (3,'Bill','Boomstick','Employee','[default profile icon here]','ABC123BCA!!!','DND','DND');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -366,4 +368,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-19  3:26:44
+-- Dump completed on 2025-04-19  8:25:07
