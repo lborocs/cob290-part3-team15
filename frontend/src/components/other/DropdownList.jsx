0e41e5b0c2f17de0f446e8daa17e9483513fcc9d
Rogger <121108364+rjansan@users.noreply.github.com>
2025-05-02 04:07:13 +0100

    attempts at blur lol

--- frontend/src/components/other/DropdownList.jsx
+++ frontend/src/components/other/DropdownList.jsx
@@ -1,20 +1,29 @@
 
 import DropdownItem from './DropdownItem';
 
-function DropdownList({ items, onClick, icons, refs, floatingStyles }) {
+function DropdownList({ items, onClick, icons, refs, floatingStyles, blurBackground }) {
   return (
-    <div
-    className="w-auto absolute bg-white rounded-lg p-2 z-50 border border-gray-300" {...(refs?.setFloating ? { ref: refs.setFloating } : {})} {...(floatingStyles ? { style: floatingStyles } : {})}>
-        {/* Goes through each item in the list and maps items to a key value*/}
-        {items.map((item, index) => (
-            <DropdownItem 
-            key={index} 
-            item={item} 
-            onClick={onClick[index]} // Maps the item to the function at the same index
-            icon={icons? icons[index] : null} // Maps the icon to the item after checking if it exists, else sets it to null
-            /> 
-        ))}
-    </div>
+    <>
+      {/* Background Blur */}
+      {blurBackground && (
+        <div
+          className="fixed inset-0 bg-black/20 backdrop-blur-sm z-10 pointer-events-none"
+        ></div>
+      )}
+
+      <div
+      className="w-auto absolute bg-white rounded-lg p-2 z-50 border border-gray-300" {...(refs?.setFloating ? { ref: refs.setFloating } : {})} {...(floatingStyles ? { style: floatingStyles } : {})}>
+          {/* Goes through each item in the list and maps items to a key value*/}
+          {items.map((item, index) => (
+              <DropdownItem 
+              key={index} 
+              item={item} 
+              onClick={onClick[index]} // Maps the item to the function at the same index
+              icon={icons? icons[index] : null} // Maps the icon to the item after checking if it exists, else sets it to null
+              /> 
+          ))}
+      </div>
+    </>
   );
 }
 
