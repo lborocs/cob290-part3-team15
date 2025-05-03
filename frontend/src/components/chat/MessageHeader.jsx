c4beb3c516b1a6f8380e005f592540dfeaa91948
Ryan17909 <ravit290st@gmail.com>
2025-05-03 23:00:01 +0100

    removed status and update profile

--- frontend/src/components/chat/MessageHeader.jsx
+++ frontend/src/components/chat/MessageHeader.jsx
@@ -3,14 +3,17 @@ import ProfileCard from "../accounts/ProfileCard";
 // components/chat/core/Header.jsx
 export default function Header({ name, selectedID, mode }) {
     // You can customize this logic however you want
-   
+   let group = ""
+
 
     return (
-        <div className="bg-accentWhite w-full h-[100px] flex items-center px-4">
-            <div className="text-lg font-semibold">
-                <div>User: {name}</div>
-                <ProfileCard displayBG={"bg-accentOrange group-hover:bg-orangeHover"} type={""} id={selectedID} status={"offline"}/>
-
+        <div className="w-full flex justify-center items-center py-2 bg-[#e3ebeb]">
+            <div className="flex items-center gap-2 text-lg font-semibold">
+                <div className="w-10 h-10 rounded-full overflow-hidden">
+                    <ProfileCard displayBG={"bg-accentOrange group-hover:bg-orangeHover"} type={mode === "group_messages"? "Group" : ""} id={selectedID} status={""}/>
+                </div>
+                <div>{name}</div>
+                
             </div>
         </div>
     );
