731e4e63154bb71267c3ea88de42e44a62af2dd9
Rogger <121108364+rjansan@users.noreply.github.com>
2025-05-03 23:38:07 +0100

    fixed offset and overflowing due to ref issues

--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -15,7 +15,7 @@ function Content({ message }) {
   );
 }
 
-function SelfMessage({ message,mode, setEditing, setEditingMessage, editingMessage, refs, floatingStyles, isDropdownOpen, toggleDropdown }) {
+function SelfMessage({ message,mode, setEditing, setEditingMessage, editingMessage, isDropdownOpen, toggleDropdown, boundaryRef }) {
   const [isHovered, SetisHovered] = useState(false); // Default is not hovered
   const [isHideModalOpen, setIsHideModalOpen] = useState(false); // State to control the modal
   const [messageToHide, setMessageToHide] = useState(null); // State to store the message to be hidden
@@ -64,11 +64,11 @@ function SelfMessage({ message,mode, setEditing, setEditingMessage, editingMessa
   // Set up floating ui
   const { refs: dropdownRefs, floatingStyles: dropdownStyles, context } = useFloating({
     middleware: [
-      offset(10), 
+      offset(), 
       flip(),         
-      shift()
+      shift({ boundary: boundaryRef.current, padding: 8})
     ],
-    placement: "bottom-end",
+    placement: "top-end",
     whileElementsMounted: autoUpdate,
     open: isDropdownOpen,
     onOpenChange: (open) => {
@@ -109,51 +109,52 @@ function SelfMessage({ message,mode, setEditing, setEditingMessage, editingMessa
           <p className="text-text text-xs ml-3 self-end font-light pb-[2px]">{formattedTime}</p>
         </div>
         }
-      <div className={`${editingMessage?.messageID == message.messageID ? "border-1 border-green-400 ": ""} mb-2 rounded-lg border border-2 border-accentGreen/80 px-4 py-2 text-base font-medium bg-accentGreen/50 relative`} 
-      onMouseEnter={HandleHover} onMouseLeave={HandleHover} onContextMenu={HandleRightClick} ref={(node) => {messageRef.current = node; if (refs.setReference){refs.setReference(node)};}}>
-
-        <div className="self-start text-pretty break-all">
-          {isHovered && (
-            <MessageOptions sentByUser={true} 
-            isHoveredComment={isHovered}
-            message={message} // Pass the message to the options
-            setEditing={setEditing} // Pass the setMessage function to the options
-            setEditingMessage={setEditingMessage} // Pass the setMessage function to the options
-            setIsDropdownOpen={toggleDropdown}
-            />
-          )}
-          <Content message={message}/>
-          {message.isEdited==1 && (<p className="text-right text-xs text-light text-gray-400 select-none">edited</p>)}
+        <div ref={messageRef}>
+          <div className={`${editingMessage?.messageID == message.messageID ? "border-1 border-green-400 ": ""} mb-2 rounded-lg border border-2 border-accentGreen/80 px-4 py-2 text-base font-medium bg-accentGreen/50 relative`} 
+          onMouseEnter={HandleHover} onMouseLeave={HandleHover} onContextMenu={HandleRightClick} ref={dropdownRefs.setReference}>
+            <div className="self-start text-pretty break-all">
+              {isHovered && (
+                <MessageOptions sentByUser={true} 
+                isHoveredComment={isHovered}
+                message={message} // Pass the message to the options
+                setEditing={setEditing} // Pass the setMessage function to the options
+                setEditingMessage={setEditingMessage} // Pass the setMessage function to the options
+                setIsDropdownOpen={toggleDropdown}
+                />
+              )}
+              <Content message={message}/>
+              {message.isEdited==1 && (<p className="text-right text-xs text-light text-gray-400 select-none">edited</p>)}
+            </div>
+            {isDropdownOpen && ( // Dropdown menu for right click options
+              <ChatDropdown
+                sentByUser={true}
+                onClose = {()=> {toggleDropdown(null); SetisHovered(false)}}
+                message={message} // Pass the message to the dropdown
+                setEditing={setEditing}
+                setEditingMessage={setEditingMessage} // Pass the setMessage function to the options
+                refs={dropdownRefs} 
+                floatingStyles={dropdownStyles}
+                openHideModal={openHideModal} // Pass the openHideModal function to the dropdown
+              />
+            )}
+            {
+              isHideModalOpen && (
+              <HideMessageModal
+                open={isHideModalOpen}
+                onClose={closeHideModal}
+                message={messageToHide}
+              />
+              )
+            }
+              
+          </div>
         </div>
-        {isDropdownOpen && ( // Dropdown menu for right click options
-          <ChatDropdown
-            sentByUser={true}
-            onClose = {()=> {toggleDropdown(null); SetisHovered(false)}}
-            message={message} // Pass the message to the dropdown
-            setEditing={setEditing}
-            setEditingMessage={setEditingMessage} // Pass the setMessage function to the options
-            refs={dropdownRefs} 
-            floatingStyles={dropdownStyles}
-            openHideModal={openHideModal} // Pass the openHideModal function to the dropdown
-          />
-        )}
-        {
-          isHideModalOpen && (
-          <HideMessageModal
-            open={isHideModalOpen}
-            onClose={closeHideModal}
-            message={messageToHide}
-          />
-          )
-        }
-          
-      </div>
       </div>
     </div>
   )
 }
 
-function OtherMessage({ message, refs, floatingStyles, isDropdownOpen, toggleDropdown }) {
+function OtherMessage({ message, isDropdownOpen, toggleDropdown, boundaryRef }) {
   const [isHovered, SetisHovered] = useState(false); // Default is not hovered]
   const messageRef = useRef(null); // Reference to the message element
   const HandleHover = (e) => {
@@ -188,11 +189,11 @@ function OtherMessage({ message, refs, floatingStyles, isDropdownOpen, toggleDro
  // Set up floating ui
  const { refs: dropdownRefs, floatingStyles: dropdownStyles, context } = useFloating({
   middleware: [
-    offset(10), 
+    offset(), 
     flip(),         
-    shift()
+    shift({ boundary: boundaryRef.current, padding: 8})
   ],
-  placement: "bottom-end",
+  placement: "top-end",
   whileElementsMounted: autoUpdate,
   open: isDropdownOpen,
   onOpenChange: (open) => {
@@ -234,32 +235,34 @@ useDismiss(context, {
           <p className="text-text text-xs ml-3 self-end font-light pb-[2px]">{formattedTime}</p>
         </div>
         }
-        <div className={`mb-2 w-fit rounded-lg border border-2 border-gray-400/20 px-4 py-2 bg-secondary relative`} onMouseEnter={HandleHover} onMouseLeave={HandleHover} onContextMenu={HandleRightClick} ref={(node) => {messageRef.current = node; if (refs.setReference){refs.setReference(node)};}}>
-          <div className="text-left flex flex-col text-pretty break-all">
-            {isHovered && (
-              <MessageOptions sentByUser={false} 
-              isHoveredComment={isHovered} 
-              message={message} // Pass the message to the options
-              setEditing={null}
-              setEditingMessage={null}
-              setIsDropdownOpen={toggleDropdown}
-              />
-            )}
-            <Content message={message}/>
-            {message.isEdited==1 && (<p className="text-right text-xs font-light text-gray-400 select-none">edited</p>)}
+        <div ref={messageRef}>
+          <div className={`mb-2 w-fit rounded-lg border border-2 border-gray-400/20 px-4 py-2 bg-secondary relative`} onMouseEnter={HandleHover} onMouseLeave={HandleHover} onContextMenu={HandleRightClick} ref={dropdownRefs.setReference}>
+            <div className="text-left flex flex-col text-pretty break-all">
+              {isHovered && (
+                <MessageOptions sentByUser={false} 
+                isHoveredComment={isHovered} 
+                message={message} // Pass the message to the options
+                setEditing={null}
+                setEditingMessage={null}
+                setIsDropdownOpen={toggleDropdown}
+                />
+              )}
+              <Content message={message}/>
+              {message.isEdited==1 && (<p className="text-right text-xs font-light text-gray-400 select-none">edited</p>)}
+            </div>
           </div>
+          {isDropdownOpen && ( // Dropdown menu for right click options
+            <ChatDropdown
+            sentByUser={false}
+            onClose = {()=> {toggleDropdown(null); SetisHovered(false)}}
+            message={message} // Pass the message to the dropdown
+            setEditing={null}
+            setEditingMessage={null} // Pass the setMessage function to the options
+            refs={dropdownRefs}
+            floatingStyles={dropdownStyles}
+            />
+          )}
         </div>
-        {isDropdownOpen && ( // Dropdown menu for right click options
-          <ChatDropdown
-          sentByUser={false}
-          onClose = {()=> {toggleDropdown(null); SetisHovered(false)}}
-          message={message} // Pass the message to the dropdown
-          setEditing={null}
-          setEditingMessage={null} // Pass the setMessage function to the options
-          refs={dropdownRefs}
-          floatingStyles={dropdownStyles}
-          />
-        )}
       </div>
     </div>
   )
@@ -273,19 +276,13 @@ useDismiss(context, {
 function Message({ messageContent , userID , mode, setEditing, setEditingMessage, editingMessage, boundaryRef, isDropdownOpen, toggleDropdown}) {
   //const [message,setMessage]=useState(messageContent);
 
-  //Refs for modal handling
-  const { refs, floatingStyles } = useFloating({
-    middleware: [offset(0), flip(), shift({ boundary: boundaryRef.current, padding: 8 })],
-    placement: 'top-end',
-  });
-
   const sentByUser = parseInt(messageContent.user) === parseInt(userID); // Check if the message was sent by the user, parses as int and uses base 10 (denary/decimal)
   return (
     <>
       {sentByUser ? 
       <SelfMessage message={messageContent} mode={mode} 
-      setEditing={setEditing} setEditingMessage={setEditingMessage} editingMessage={editingMessage} refs={refs} floatingStyles={floatingStyles} isDropdownOpen={isDropdownOpen} toggleDropdown={toggleDropdown}/> 
-      : <OtherMessage message={messageContent} refs={refs} floatingStyles={floatingStyles} isDropdownOpen={isDropdownOpen} toggleDropdown={toggleDropdown}/>}
+      setEditing={setEditing} setEditingMessage={setEditingMessage} editingMessage={editingMessage} isDropdownOpen={isDropdownOpen} toggleDropdown={toggleDropdown} boundaryRef={boundaryRef}/> 
+      : <OtherMessage message={messageContent} isDropdownOpen={isDropdownOpen} toggleDropdown={toggleDropdown} boundaryRef={boundaryRef}/>}
     </>
   );
 }
